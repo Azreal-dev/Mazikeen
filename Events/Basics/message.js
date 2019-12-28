7009c7aeb1006195d67918b4412df63b89ffc0e2
Atlass_75 <p.atlass75@gmail.com>
2019-12-29 00:21:17 +0100

    Update message.js

--- Events/Basics/message.js
+++ Events/Basics/message.js
@@ -8,6 +8,10 @@ module.exports = async (client, msg) => {
     try {
         const prefix = CFG.prefix;
         const content = msg.content.toLowerCase();
+        if (content.includes("es tu là mazikeen") {
+            msg.react("✅");
+            return msg.reply("Oui, je suis là ☺");
+        }
         if (content.includes('je t\'aime mazikeen')) {
             if (CFG.love.blacklist.find(id => id === msg.author.id)) new Messages(msg, client, false).send('T\'es qui toi connard ? :middle_finger:');
             else if (CFG.love.whitlist.find(id => id=== msg.author.id)) new Messages(msg, client, null).send('Je t\'aime aussi mon ange déchu :heart:');
